ca842730562bc59268e4e91f6a09ba841084513b
Darshan Gawande <gawanded24@gmail.com>
2023-04-19 01:01:21 +0530

    done

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
-var buttonSelector = document.querySelector("#btn-translator");
-var textInput = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translator");
+var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
 
-function clickEventHandler() {
-   outputDiv.innerText = "ajnskankdsngskna = " + textInput.value;
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL = "https://api.funtranslations.com/translate/dothraki.json";
+
+
+function getTranslationURL(text) {
+   return serverURL + "?" + "text=" + text
 }
 
-buttonSelector.addEventListener("click",clickEventHandler);
+function errorHandler(error) {     // error handling
+   console.log("error occurred",error);
+   alert("something wrong with server, again after some time ");
+}
+
+
+function clickEventHandler() {
+   var inputText = txtInput.value;
+
+   fetch(getTranslationURL(inputText))
+   .then(response => response.json())
+   .then(json => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+   })
+   .catch(errorHandler)
+   
+}
+
+btnTranslate.addEventListener("click",clickEventHandler);
 
 
